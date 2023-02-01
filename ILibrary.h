2ae0e5eaded50baad524a3e2f656944ec51b65af
Ж - 5 < - 5@DESKTOP-6T70S6U>
2023-02-01 21:25:46 +0300

    Add project files.

---
+++ ILibrary.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Book.h"
+#include "Subscriber.h"
+class ILabriry{
+public:
+	virtual void AddBook(const Book& book) = 0;
+	virtual void ModifyBook(int key) = 0;
+	virtual void removeBook(int key) = 0;
+	virtual void RegistrationSubscriber(const Subscriber& user) = 0;
+	virtual ~ILabriry() {}
+};
---
+++ Library.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include "ILibrary.h"
+#include "string"
+#include <map>
+#include <queue>
+class Library: public ILabriry{
+public:
+	Library(const std::string& name);
+	void AddBook(const Book& book) override ;
+	void ModifyBook(int key) override;
+	void removeBook(int key) override;
+	void RegistrationSubscriber(const Subscriber& user) override;
+	void Pop();
+private:
+	std::string _name;
+	std::map<int, Book> _books;
+	std::queue<Subscriber> _subsribers;
+	unsigned int _id_book;
+};
+
